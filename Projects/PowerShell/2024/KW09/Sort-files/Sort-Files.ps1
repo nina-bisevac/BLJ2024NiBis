cf4307fa9bf020afe5a5f74e38be903242733443
Nina Bisevac <nina.bisevac@noseryoung.com>
2024-03-06 12:05:11 +0100

    SortFiles Code Update

--- Projects/PowerShell/2024/KW09/Sort-files/Sort-Files.ps1
+++ Projects/PowerShell/2024/KW09/Sort-files/Sort-Files.ps1
@@ -1,25 +1,40 @@
 # Erstellen Sie einen Ordner TestingPurpose mit 2 Unterordner SubFolder1 und SubFolder2
-New-Item -ItemType Directory -Path ".\TestingPurpose\SubFolder1" -Force
-New-Item -ItemType Directory -Path ".\TestingPurpose\SubFolder2" -Force
+New-Item -ItemType Directory -Path "C:\Users\ninab\BLJ2024NiBis\Projects\PowerShell\2024\KW09\Sort-files\TestingPurpose\SubFolder1" -Force
+New-Item -ItemType Directory -Path "C:\Users\ninab\BLJ2024NiBis\Projects\PowerShell\2024\KW09\Sort-files\TestingPurpose\SubFolder2" -Force
 
 # Erstellen Sie einige Testdateien in diesen Ordnern:
 # 	TypeATest1.txt, TypeATest2.txt ... TypeATest50.txt in SubFolder1
 # 	TypeBTest51.txt, TypeBTest52.txt ... TypeBTest100 in SubFolder2 
+for ($i = 1; $i -le 50; $i++) {
+    $fileName = "TypeATest$i.txt"
+    $filePath = "C:\Users\ninab\BLJ2024NiBis\Projects\PowerShell\2024\KW09\Sort-files\TestingPurpose\SubFolder1\$fileName"
+
+    New-Item -ItemType File -Path $filePath -Force
+}
+
+for ($i = 51; $i -le 100; $i++) {
+    $fileName = "TypeBTest$i.txt"
+    $filePath = "C:\Users\ninab\BLJ2024NiBis\Projects\PowerShell\2024\KW09\Sort-files\TestingPurpose\SubFolder2\$fileName"
+
+    New-Item -ItemType File -Path $filePath -Force
+}
+
 
 # Verschiebe alle Dateien, die eine ungerade Zahl im Namen haben, in SubFolder2.
 # Verschiebe alle Dateien, die eine gerade Zahl im Namen haben, in SubFolder1.
-$path = C:\Users\ninab\BLJ2024NiBis\Projects\PowerShell\2024\KW09\Sort-files\TestingPurpose
+$path = "C:\Users\ninab\BLJ2024NiBis\Projects\PowerShell\2024\KW09\Sort-files\TestingPurpose"
+
 ls C:\Users\ninab
 Get-ChildItem -Path $path -Recurse -File |  
     ForEach-Object {
         $zahl = ($_.Name -replace '\D','').Trim() 
+        $name = $_.Name
         if ($zahl % 2 -eq 0) {
-            Move-Item -path $path\SubFolder2\TypeATest$zahl.txt -Destination $path\SubFolder1
+            Move-Item -path $path\SubFolder2\$name -Destination $path\SubFolder1
         }else {
-            Move-Item -path $path\TypeATest$zahl.txt -Destination $path\SubFolder2
+            Move-Item -path $path\SubFolder1\$name -Destination $path\SubFolder2
         }    
-            }
-
+    }
 
 #Benenne den Ordner SubFolder1 in EvenFilesContainer und SubFolder2 in OddFilesContainer um.
 #Rename-Item -Path ".\TestingPurpose\SubFolder1" -NewName "EvenfilesContainer" -Force
@@ -36,5 +51,3 @@ Get-ChildItem -Path $path -Recurse -File |
 
 # Löschen Sie alle Dateien, die mit TypeA beginnen
 #Remove-Item -Path ".\TestingPurpose\EvenFilesContainer\TypeA*.txt" -Force
-
-
